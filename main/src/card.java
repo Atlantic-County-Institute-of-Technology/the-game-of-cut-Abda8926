769d68087515ac9aa3e9ee98b70d1465c8664946
Youssief Abdalla <127241459+Abda8926@users.noreply.github.com>
2025-10-28 12:56:29 -0400

    Update card.java

--- main/src/card.java
+++ main/src/card.java
@@ -2,6 +2,19 @@ public class card {
     public final int face;
     private final int suit;
     private final String[] faces = {null, "Ace", "2", "3", "4", "5", "7" ,"8", "9", "10", "Jack", "Queen", "King"};
-    private final String[] suits = {null, ""}
+    private final String[] suits = {null, "♠", "♥", "♣", "♦" };
+
+    public card(int face, int suit) {
+        this.face = face;
+        this.suit = suit;
+    }
+
+    public int getFace() {
+        return faces[face];
+    }
+
+    public int getSuit() {
+        return suits[suit];
+    }
 }
 
