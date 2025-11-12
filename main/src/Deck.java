0fbbb8e5d76436c17c719e951ab0ccc6071986a2
Youssief Abdalla <127241459+Abda8926@users.noreply.github.com>
2025-11-12 12:57:18 -0500

    oh my god bruh
    what

--- main/src/Deck.java
+++ main/src/Deck.java
@@ -1,7 +1,9 @@
+import java.util.*;
 public class Deck {
-    public static card[] popDeck() {
+    Random shuffler = new Random();
+    card[] deck = new card[52];
+    public card[] popDeck() {
         int i = 0;
-        card[] deck = new card[52];
         for (int suit = 1; suit <= 4; suit++) {
             for (int face = 1; face <= 13; face++) {
                 deck[i] = (new card(face, suit));
@@ -9,4 +11,25 @@ public class Deck {
             }
         }
         return deck;}
+    
+    public void shuffleDeck() {
+        for(int i = 0; i < 52; i++) {
+            int newPos = shuffler.nextInt(52);
+            card temp = deck[i];
+            deck[i] = deck[newPos];
+            deck[newPos] = temp;
+        }
+    }
+
+    public card fetchCard(int target){
+
+        card ret = deck[target];
+        
+    }
+
+    public card draw(){
+
+    }
+    
 }
+
